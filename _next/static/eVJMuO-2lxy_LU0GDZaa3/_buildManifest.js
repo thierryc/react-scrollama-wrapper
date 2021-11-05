b1837e2749cc58c88a5b9aa932a703db845e9ddb
Thierry Charbonnel <t@ap.cx>
2021-11-05 18:22:23 -0400

    Updates

---
+++ _next/static/eVJMuO-2lxy_LU0GDZaa3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/091b42caae80d757.css","static/chunks/pages/index-a9090f01526452e3.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
